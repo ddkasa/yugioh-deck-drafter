fc7bc101c97138afcfc00ef072dd1934c6671e7e
DK <davidakasakaitis@gmail.com>
2023-11-11 22:05:56 +0000

    Basic Set Functionaliy

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # yugioh-deck-drafter
 Python app for drafting custom apps with the Ygoprodeck API.
 
-
+## Custom Ruleset
 Draw to 20 > Side 2 -> Discard 8 -> (10+2) 
 Draw to 30 > Side 2 -> Discard 8 -> (20+4) 
 Draw to 40 > Side 2 -> Discard 8 -> (30+6) 
 Draw to 50 > Side 2 -> Discard 8 -> (40+8) 
+
+Polymerization -> Take up Card
+Type -> Same Type Card -> Limit After
+
+# API USED
+https://ygoprodeck.com/api-guide/
--- yugioh_deck_drafter/__main__.py
+++ yugioh_deck_drafter/__main__.py
@@ -22,7 +22,6 @@ from PyQt6.QtGui import (QIntValidator)
 
 import requests_cache
 
-
 NAME = "YU-GI-OH Deck Creator"
 
 
@@ -52,19 +51,34 @@ class YugiObj:
         new_set.sort(key=lambda x: x["tcg_date"])
         return new_set
 
+    def get_card_set_info(self, card_set: str) -> list:
+        INFO = f"https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset={card_set}"
+        request = self.CACHE.get(INFO)
+        if request.status_code != 200:
+            logging.critical("Failed to fetch Card Sets. Exiting!")
+            logging.critical(request.status_code)
+            sys.exit()
+        return request.json()
+
+    def get_cards(self, card_set: int) -> list:
+        CARD_SET = "https://db.ygoprodeck.com/api/v7/cardinfo.php?banlist=tcg&level=4&sort=name"
+        data = []
+        return
+
 
 class MainWindow(QWidget):
 
-    def __init__(self,
-                 parent: QWidget | None,
-                 flags=Qt.WindowType.Widget) -> None:
-        super().__init__(parent, flags)
+    def __init__(self, parent: QWidget | None, flags=Qt.WindowType.Widget):
+        super(MainWindow, self).__init__(parent, flags)
         self.YU_GI = YugiObj()
 
         self.setWindowTitle(NAME)
         self.selected_sets = {}
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
+
+        self.selected_cards = []
+        self.side_deck = {}
 
         self.select_layout = QHBoxLayout()
         self.main_layout.addLayout(self.select_layout)
@@ -76,7 +90,7 @@ class MainWindow(QWidget):
         self.select_layout.addStretch(10)
 
         self.no_packs = QSlider()
-        self.no_packs.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        self.no_packs.setTickPosition(QSlider.TickPosition.NoTicks)
         self.no_packs.setTickInterval(5)
         self.no_packs.setOrientation(Qt.Orientation.Horizontal)
         self.no_packs.setMinimum(1)
@@ -90,20 +104,38 @@ class MainWindow(QWidget):
         self.list_widget = QListWidget()
         self.main_layout.addWidget(self.list_widget)
 
+        self.button_layout = QHBoxLayout()
         self.start_button = QPushButton("START")
-        self.main_layout.addWidget(self.start_button)
+        self.button_layout.addWidget(self.start_button)
+        self.button_layout.addStretch(20)
+        self.reset_button = QPushButton("RESET")
+        self.button_layout.addWidget(self.reset_button)
+        self.reset_button.pressed.connect(self.reset_selection)
+
+        self.main_layout.addLayout(self.button_layout)
+
         self.select_pack.currentIndexChanged.connect(self.add_item)
         self.no_packs.valueChanged[int].connect(self.update_indi)
+        self.no_pack_indi.textChanged[str].connect(self.update_indi)
+        self.start_button.pressed.connect(self.start_creating)
+
         self.show()
 
     def add_item(self):
         label = self.select_pack.currentText()
-        cnt = self.no_packs.value()
+        index = self.select_pack.currentIndex()
+        cnt = max(int(self.no_pack_indi.text()), 1)
+
         item = QListWidgetItem(f"{cnt}x {label}")
         self.list_widget.addItem(item)
 
-    def update_indi(self, value: int):
-        self.no_pack_indi.setText(str(value))
+        self.selected_sets[label] = (cnt, self.YU_GI.card_set[index])
+
+    def update_indi(self, value: int | str):
+        if isinstance(value, int):
+            self.no_pack_indi.setText(str(value))
+            return
+        self.no_packs.setValue(int(value))
 
     def start_creating(self):
         dialog = SelectioDialog(self)
@@ -111,16 +143,51 @@ class MainWindow(QWidget):
             # Save the deck here
             pass
 
+    def reset_selection(self):
+        logging.info("Resetting app to defaults.")
+
+        self.selected_cards = []
+        self.selected_side = []
+
+        self.list_widget.clear()
+
 
 class SelectioDialog(QDialog):
 
     def __init__(self, parent: MainWindow, flags=Qt.WindowType.Dialog):
-        super().__init__(parent, flags)
+        super(SelectioDialog, self).__init__(parent, flags)
+        self.setWindowTitle("Card Selector")
+
+        self.deck = parent.selected_sets
+        self.data_requests = parent.YU_GI
 
         self.main_layout = QVBoxLayout()
+
         self.card_layout = QGridLayout()
+
+        self.button_layout = QHBoxLayout()
+        self.cancel_button = QPushButton("Cancel")
+        self.cancel_button.pressed.connect(self.reject)
+        self.button_layout.addWidget(self.cancel_button)
+
+        self.button_layout.addStretch(80)
+
+        self.accept_button = QPushButton("Accept")
+        self.accept_button.pressed.connect(self.accept)
+        self.button_layout.addWidget(self.accept_button)
+
+        self.main_layout.addLayout(self.button_layout)
+
         self.setLayout(self.main_layout)
 
+        test_set_key = list(self.deck.keys())[0]
+        self.open_pack(test_set_key, self.deck[test_set_key])
+
+    def open_pack(self, card_set_name: str, card_set: tuple):
+        print(card_set)
+        data = self.data_requests.get_card_set_info(card_set_name)
+        # card_set[1]["set_code"]
+        pprint(data)
 
 
 def main():
